61833fea2c88485552f074b9a0b66ca15f909315
sihuihui <my.email.is.sihui@gmail.com>
2024-03-11 23:12:44 +0800

    updates to takehome ex4

---
+++ Take-home_Ex/Take-home_Ex04/app.R
@@ -0,0 +1,81 @@
+pacman::p_load(shiny,tidyverse, lubridate, knitr, DT, ggplot2, plotly, ggthemes, 
+               ggfortify, forecast, MLmetrics, tsbox, xts, imputeTS, tseries, hrbrthemes)
+
+temp <-read_rds("data/monthly_temp.rds")
+
+ui <- fluidPage(
+    titlePanel("Prototype for Forecasting Module"),
+    sidebarLayout(
+        sidebarPanel(
+          selectInput(inputId = "shstation",
+                      label = "Select a weather station",
+                      choices = c("Admiralty" = "Admiralty",
+                                  "Ang Mo Kio" = "Ang Mo Kio",
+                                  "Changi" = "Changi", 
+                                  "Choa Chu Kang (South)" = "Choa Chu Kang (South)",
+                                  "Clementi" = "Clementi",
+                                  "East Coast Parkway" = "East Coast Parkway",
+                                  "Jurong Island" = "Jurong Island",
+                                  "Jurong (West)" = "Jurong (West)",
+                                  "Newton" = "Newton",
+                                  "Pasir Panjang" = "Pasir Panjang", 
+                                  "Sentosa Island"  = "Sentosa Island",
+                                  "Tai Seng" = "Tai Seng",
+                                  "Tuas South" = "Tuas South"),
+                      selected = "Admiralty"),
+          hr(),
+          selectInput(inputId = "shtemp",
+                      label = "Select the type of temperature reading to forecast",
+                      choices = c("Mean Temperature" = "mean_temperature",
+                                  "Maximum Temperature" = "maximum_temperature",
+                                  "Minimum Temperature" = "minimum_temperature"),
+                      selected = "mean_temperature"),
+          hr(),
+          radioButtons("shdataimpute", 
+                        "Select a Missing Data Imputation Method",
+                       c("Moving Average (Exponential)" = "exponential",
+                         "Moving Average (Linear)" = "linear",
+                         "Moving Average Simple)" = "simple",
+                         "Kalman Smoothing (ARIMA)" = "auto.arima",
+                         "Kalman Smoothing (StrucTS)" = "StructTS")),
+          hr(),
+          radioButtons("shforecast", 
+                       "Select a Forecasting Method",
+                       c("State Space Model" = "ets",
+                         "Holt-Winters' Model (Additive Seaonality)" = "hwa",
+                         "Holt-Winters' Model (Multiplicative Seaonality)" = "hwm",
+                         "ARIMA" = "arima")),
+          hr(),
+          sliderInput(inputId = "forecastduration",
+                      label = "Select the number of months to forecast",
+                      min = 1,
+                      max = 120,
+                      value = 24,
+                      step = 1),
+          hr(),
+          actionButton(inputId = "temp_forecast",
+                       label = "Forecast Temperature Data")
+        ),
+        mainPanel("main panel",
+                  fluidRow(
+                    column(6, plotOutput(outputId = "tempdata",
+                                         width = "300px",
+                                         height = "300px")),
+                    column(6, plotOutput(outputId = "forecastdata",
+                                         width = "300px",
+                                         height = "300px")),
+                    
+                  )
+        )
+    )
+)
+
+
+server <- function(input, output) {
+
+
+    }
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
