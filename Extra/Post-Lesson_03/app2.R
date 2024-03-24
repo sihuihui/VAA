f70b93bddcd5ff30eea8751aef80e09df261dfca
sihuihui <my.email.is.sihui@gmail.com>
2024-03-24 09:37:58 +0800

    fixed app1.R bug

--- Extra/Post-Lesson_03/app2.R
+++ Extra/Post-Lesson_03/app2.R
@@ -109,67 +109,17 @@ ui <- fluidPage(
              ), 
              mainPanel(
                plotOutput("sh_SelectedDataPlot"),
-               DT::dataTableOutput("sh_SelectedDataTable"),
                plotOutput("sh_DecompositionPlot"),
                plotOutput("sh_ValidationPlot"),
+               plotOutput("sh_ForecastPlot"),
+               plotOutput("sh_ResidualsPlot"),
                DT::dataTableOutput("sh_AccuracyTable"),
-               plotOutput("sh_ForecastPlot")
+               DT::dataTableOutput("sh_SelectedDataTable")
              )
            )),
-  
-##### ARIMA ######
-  tabPanel("ARIMA",
-           sidebarLayout(
-             sidebarPanel(
-               selectInput(inputId = "sh_station",
-                           label = "Select a weather station",
-                           choices = c("Admiralty" = "Admiralty",
-                                       "Ang Mo Kio" = "Ang Mo Kio",
-                                       "Changi" = "Changi", 
-                                       "Choa Chu Kang (South)" = "Choa Chu Kang (South)",
-                                       "Clementi" = "Clementi",
-                                       "East Coast Parkway" = "East Coast Parkway",
-                                       "Jurong Island" = "Jurong Island",
-                                       "Jurong (West)" = "Jurong (West)",
-                                       "Newton" = "Newton",
-                                       "Pasir Panjang" = "Pasir Panjang", 
-                                       "Sentosa Island"  = "Sentosa Island",
-                                       "Tai Seng" = "Tai Seng",
-                                       "Tuas South" = "Tuas South"),
-                           selected = "Admiralty"),
-               hr(),
-               selectInput(inputId = "sh_variable",
-                           label = "Select the variable to forecast",
-                           choices = c("Mean Temperature" = "mean_monthly_temperature",
-                                       "Maximum Temperature" = "max_monthly_temperature",
-                                       "Minimum Temperature" = "min_monthly_temperature",
-                                       "Total Rainfall" = "monthly_rainfall"),
-                           selected = "mean_monthly_temperature"),
-               
-               hr(), 
-               checkboxGroupInput(inputId = "sh_decompose",
-                                  label = "Select the STL decompositions to visualise",
-                                  choices = c("Observed" = "observed",
-                                              "Season" = "season",
-                                              "Trend" = "trend",
-                                              "Remainder" = "remainder",
-                                              "Seasonal Adjusted" = "seasadj")),
-               
-               hr(),
-               actionButton(inputId = "sh_plotdata",
-                            label = "Show my Data!"),
-               
-               
-             ), 
-             mainPanel(
-               plotOutput("sh_SelectedDataPlot"),
-               plotOutput("sh_DecompositionPlot"),
-               plotOutput("sh_ValidationPlot"),
-               DT::dataTableOutput("sh_AccuracyTable"),
-               plotOutput("sh_ForecastPlot")
-             ))
+  tabPanel("ARIMA")
   )
-))
+)
 
 
 server <- function(input, output) {
@@ -197,12 +147,11 @@ server <- function(input, output) {
     #selectedData()
     #})
   
-  output$sh_DecompositionPlot <- renderPlot({
-    req(selectedData())
-    plot_stl_diagnostics(selectedData(), date, value,
-                         .feature_set = input$sh_decompose,
-                         .interactive = FALSE)
-  })
+  #output$sh_DecompositionPlot <- renderPlot({
+    #req(selectedData())
+    #timetk::plot_stl_diagnostics(selectedData(), as.Date(tdate), input$sh_variable,
+                         #.feature_set = input$sh_decompose)
+  #})
     
 
 ## split the data based on the training proportion chosen 
@@ -210,6 +159,14 @@ server <- function(input, output) {
     req(selectedData())
     initial_time_split(selectedData(), prop = input$sh_traindata)
   })
+  
+  recipe_trg <- reactive({
+    recipe(value ~ date, training(splits())) %>%
+      step_dummy(all_nominal())
+  })
+  
+  #recipe_trg <- recipe_trg %>% 
+    #step_dummy(all_nominal())
   
   train_data <- reactive({ 
     training(splits()) 
@@ -233,71 +190,18 @@ server <- function(input, output) {
   })
 
 ## Add Fitted Model to a Model table 
-  model_ets_table <- reactive({
-    req(model_fit_ets())
-    modeltime_table(model_fit_ets())
-  })
-  
+  model_ets_table <- modeltime_table(model_fit_ets)
 
 ## Calibrate model to test data
-  calibration_ets <- reactive({
-    req(model_ets_table(), test_data())
-    model_ets_table() %>%
+  calibration_ets <- model_ets_table %>%
       modeltime_calibrate(new_data = test_data())
-  })
 
-  calibration_results <- reactive({
-    req(calibration_ets())
-    calibration_ets() %>%
+  calibration_results <- calibration_ets %>%
       modeltime_forecast(new_data = test_data(),
                          actual_data = selectedData())
-  })
 
 ## Plot forecasted and actual test data
-  output$sh_ValidationPlot <- renderPlot({
-    req(calibration_results())
-    calibration_results() %>%
-      plot_modeltime_forecast(.interactive = FALSE)
-  })
-  
-  ## refit to full dataset & forecast forward 
-  refit <- reactive({
-    calibration_ets() %>%
-    modeltime_refit(data = selectedData())
-  })
-  
-  ## Plot the forecasted horizon 
-  output$sh_ForecastPlot <- renderPlot({
-    req(refit())
-    refit() %>%
-      modeltime_forecast(h = input$sh_forecasthorizon, actual_data = selectedData()) %>%
-      plot_modeltime_forecast(.interactive = FALSE) 
-  })
-  
 
-  calibration_table <- reactive({
-    req(model_ets_table(), test_data())
-    model_ets_table() %>%
-      modeltime_calibrate(test_data())
-  })
-  
-  forecast_table <- reactive({
-    req(calibration_table)
-    calibration_table() %>%
-      modeltime_accuracy() %>%
-      select(.model_desc, mae,mape, mase, rmse)
-  })
-
-#Show accuracy measures of forecasted test data 
-  output$sh_AccuracyTable <- DT::renderDataTable({
-    req(forecast_table())
-    forecast_table()
-  }) 
-  
- 
-  
-  
-  
   
   }
   
